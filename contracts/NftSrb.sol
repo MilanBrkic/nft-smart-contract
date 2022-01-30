538791d282b91a2207667b60f121332c6b31774a
Milan Brkic <milan.brkic1998@gmail.com>
2022-01-30 23:10:45 +0100

    adding new getAllByAddress to contract plus prettier plus tests

--- contracts/NftSrb.sol
+++ contracts/NftSrb.sol
@@ -7,11 +7,27 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NftSrb is ERC721URIStorage{
     constructor() ERC721("NftSrbija", "NFTSRB") {}
-    uint256 private _counter = 0;
+    uint256 public counter = 0;
+    mapping(uint256 => address) public owners;
+
+    event Mint(uint256 _tokenId, address minter);
 
     function mint(string memory tokenURI) public{
-        _safeMint(msg.sender, _counter);
-        _setTokenURI(_counter, tokenURI);
-        _counter++;
+        _safeMint(msg.sender, counter);
+        _setTokenURI(counter, tokenURI);
+        owners[counter] = msg.sender;
+        emit Mint(counter++, msg.sender);
+    }
+
+    function getAllByAddress(address userAddress) public view returns(uint[] memory){
+        uint numberOfTokens = balanceOf(userAddress);
+        uint[] memory userTokens = new uint[](numberOfTokens);
+        uint userCounter = 0;
+        for(uint256 i = 0;i<counter;i++){
+            if(owners[i]==userAddress){
+                userTokens[userCounter++] = i; 
+            }
+        }
+        return userTokens;
     }
 }
--- test/NftSrb.test.js
+++ test/NftSrb.test.js
@@ -1,65 +1,91 @@
-const chai = require("chai");
-const { ethers } = require("hardhat");
-chai.use(require('chai-as-promised'));
-const {assert, expect} = chai;
+const chai = require('chai')
+const { ethers } = require('hardhat')
+chai.use(require('chai-as-promised'))
+const { assert, expect } = chai
 
-describe("Nft Srbija", function () {
-  let NftSrb;
-  let nftsrb;
-  let accounts;
-  let signerAddress;
-  let signer;
+describe('Nft Srbija', function () {
+    let NftSrb
+    let nftsrb
+    let accounts
+    let signerAddress
+    let alfaSigner
+    let betaSigner
+    let gamaSigner
 
-  beforeEach(async ()=>{
-    accounts = await ethers.getSigners();
-    signer = accounts[0];
-    signerAddress = signer.address;
+    beforeEach(async () => {
+        accounts = await ethers.getSigners()
+        ;[alfaSigner, betaSigner, gamaSigner] = [accounts[0], accounts[1], accounts[2]]
+        signerAddress = alfaSigner.address
 
-    NftSrb = await ethers.getContractFactory("NftSrb", signer);
-    nftsrb = await NftSrb.deploy();
-    await nftsrb.deployed();
+        NftSrb = await ethers.getContractFactory('NftSrb', alfaSigner)
+        nftsrb = await NftSrb.deploy()
+        await nftsrb.deployed()
 
-    deployedAddress = nftsrb.address;
-  })
-
-  it("Should fail for tokenId that does not exist", async ()=>{
-    await expect(nftsrb.tokenURI(0)).to.be.rejectedWith(); 
-  })
-
-  describe("Minting process", ()=>{
-
-    it("Should mint a token", async () => {
-      const tokenUri = 'www.SomeTokenUrl.com';
-      const mintedToken = await nftsrb.mint(tokenUri);
-      const balance  = await nftsrb.balanceOf(signerAddress);
-      assert.equal(balance, 1);
-      const receivedTokenUri = await nftsrb.tokenURI(0);
-  
-      assert.equal(mintedToken.from, signerAddress);
-      assert.equal(mintedToken.to, deployedAddress);
-      assert.equal(receivedTokenUri, tokenUri);
-    });
-  
-    it("Should mint three tokens", async()=>{
-      const tokenURIS = [
-                        `www.${Math.floor(Math.random()*10000)}.com`,
-                        `www.${Math.floor(Math.random()*10000)}.com`,
-                        `www.${Math.floor(Math.random()*10000)}.com`
-                      ]
-      for(const tokenURI of tokenURIS){
-        await nftsrb.mint(tokenURI);
-      }
-  
-      const balance  = await nftsrb.balanceOf(signerAddress);
-      
-      for(let i = 0;i < tokenURIS.length;i++){
-        const ownerAddress = await nftsrb.ownerOf(i);
-        assert.equal(signerAddress, ownerAddress);
-      }
-  
-      assert.equal(balance, tokenURIS.length);
+        deployedAddress = nftsrb.address
     })
-  })
-  
 
-});
+    it('Should fail for tokenId that does not exist', async () => {
+        await expect(nftsrb.tokenURI(0)).to.be.rejectedWith()
+    })
+
+    describe('Minting process', () => {
+        it('Should mint a token', async () => {
+            const tokenUri = 'www.SomeTokenUrl.com'
+            const mintedToken = await nftsrb.mint(tokenUri)
+            const balance = await nftsrb.balanceOf(signerAddress)
+            assert.equal(balance, 1)
+            const receivedTokenUri = await nftsrb.tokenURI(0)
+
+            assert.equal(mintedToken.from, signerAddress)
+            assert.equal(mintedToken.to, deployedAddress)
+            assert.equal(receivedTokenUri, tokenUri)
+        })
+
+        it('Should mint three tokens', async () => {
+            const tokenURIS = [
+                `www.${Math.floor(Math.random() * 10000)}.com`,
+                `www.${Math.floor(Math.random() * 10000)}.com`,
+                `www.${Math.floor(Math.random() * 10000)}.com`,
+            ]
+            for (const tokenURI of tokenURIS) {
+                await nftsrb.mint(tokenURI)
+            }
+
+            const balance = await nftsrb.balanceOf(signerAddress)
+
+            for (let i = 0; i < tokenURIS.length; i++) {
+                const ownerAddress = await nftsrb.ownerOf(i)
+                assert.equal(signerAddress, ownerAddress)
+            }
+
+            assert.equal(balance, tokenURIS.length)
+        })
+    })
+
+    it('Should return a correct array of token ids', async () => {
+        await nftsrb.connect(alfaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+        await nftsrb.connect(alfaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+        await nftsrb.connect(betaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+        await nftsrb.connect(alfaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+        await nftsrb.connect(gamaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+        await nftsrb.connect(gamaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+        await nftsrb.connect(alfaSigner).mint(`www.${Math.floor(Math.random() * 10000)}.com`)
+
+        const [alfaIds, betaIds, gamaIds] = await Promise.all([
+            await nftsrb.getAllByAddress(alfaSigner.address),
+            await nftsrb.getAllByAddress(betaSigner.address),
+            await nftsrb.getAllByAddress(gamaSigner.address),
+        ]);
+
+        assert.equal(alfaIds.length,4);
+        assert.equal(alfaIds[0], 0)
+        assert.equal(alfaIds[1], 1)
+        assert.equal(alfaIds[2], 3)
+        assert.equal(alfaIds[3], 6)
+        assert.equal(betaIds.length,1);
+        assert.equal(betaIds[0],2)
+        assert.equal(gamaIds.length,2);
+        assert.equal(gamaIds[0], 4);
+        assert.equal(gamaIds[1], 5);
+    })
+})
