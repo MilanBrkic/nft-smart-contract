83c303cd1b70eef16e9deeae3df65c0ac51be493
Milan Brkic <milan.brkic1998@gmail.com>
2022-05-08 15:49:13 +0200

    removing console

--- contracts/NftSrb.sol
+++ contracts/NftSrb.sol
@@ -1,31 +1,30 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+contract NftSrb is ERC721URIStorage {
+    constructor() ERC721('NftSrbija', 'NFTSRB') {}
 
-contract NftSrb is ERC721URIStorage{
-    constructor() ERC721("NftSrbija", "NFTSRB") {}
     uint256 public counter = 0;
     mapping(uint256 => address) public owners;
 
     event Mint(uint256 _tokenId, address minter);
 
-    function mint(string memory tokenURI) public{
+    function mint(string memory tokenURI) public {
         _safeMint(msg.sender, counter);
         _setTokenURI(counter, tokenURI);
         owners[counter] = msg.sender;
         emit Mint(counter++, msg.sender);
     }
 
-    function getAllByAddress(address userAddress) public view returns(uint[] memory){
-        uint numberOfTokens = balanceOf(userAddress);
-        uint[] memory userTokens = new uint[](numberOfTokens);
-        uint userCounter = 0;
-        for(uint256 i = 0;i<counter;i++){
-            if(owners[i]==userAddress){
-                userTokens[userCounter++] = i; 
+    function getAllByAddress(address userAddress) public view returns (uint256[] memory) {
+        uint256 numberOfTokens = balanceOf(userAddress);
+        uint256[] memory userTokens = new uint256[](numberOfTokens);
+        uint256 userCounter = 0;
+        for (uint256 i = 0; i < counter; i++) {
+            if (owners[i] == userAddress) {
+                userTokens[userCounter++] = i;
             }
         }
         return userTokens;
